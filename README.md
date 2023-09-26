c143b6053d4ba52f77dc5b2bf6f7a5eda43b1cac
Saida <saida.hf@hotmail.com>
2023-09-26 21:36:54 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# StreamlitPandas
+# StreamlitPandas 🐼
+
+This is a program for practice pandas and Streamlit with **Python**
+
+## Requirements
+
+[1. Pip](https://pip.pypa.io/en/stable/)
+> py get-pip.py
+
+[3. Pandas](https://pandas.pydata.org/docs/)
+> install pandas
+
+
+[5. Streamlit](https://streamlit.io/)
+> pip install streamlit
+
+
+[7. Conda](https://docs.anaconda.com/free/anaconda/install/index.html)
+> create conda environment
+
+
+Execute program:
+
+`streamlit run ./scripts/__init__.py`
+
+
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,26 +1,22 @@
-import os
-
 import streamlit as st
 import pandas as pd
 
 st.write("""
-# My first app
-Hello *world!*
+# Hey ☜(ﾟヮﾟ☜)
+Hello world by Saida
 """)
 
 df = pd.read_csv("C:\\Users\\Saida\\Desktop\\streamlit_test\\scripts\\temperature.csv")
 df.dropna(inplace=True)
 df.reset_index(drop=True)
 
-df1 = pd.DataFrame(columns=['year'],data=df["year"])
-df2 = pd.DataFrame(columns=['temperature'],data=df["AverageTemperatureUncertaintyFahr"])
-df3 = pd.concat(objs=[df1,df2],axis=1)
+df1 = pd.DataFrame(columns=['year'], data=df["year"])
+df2 = pd.DataFrame(columns=['temperature'], data=df["AverageTemperatureUncertaintyFahr"])
+df3 = pd.concat(objs=[df1, df2], axis=1)
 
-
-# df2 = pd.DataFrame(data=[df["year"], df["AverageTemperatureUncertaintyFahr"]])
-# df2["year"] = pd.to_numeric(df2["year"])
 st.line_chart(df3)
 
-print(df.to_string())
-
+st.write("""
+What is your level in Baldur's Gate III?
+""")
 number = st.slider("Pick a number", 1, 20)
